d149cfbce0d4f88f13bf86810a9982fd59a42cf9
Jack Coyne <jack-coyne@uiowa.edu>
2024-04-08 15:05:31 -0500

    Made changes to html files and edits to app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "hello world, index"
+def home():
+    return render_template('index.html')
 
-@app.route('/scott')
-def scott():
-    return "hello world, scott"
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
